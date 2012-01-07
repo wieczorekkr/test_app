c15d73ee231c4550bd7154bea1e5a6ead6c0fdee
mav <wieczorekkr@gmail.com>
2012-01-07 14:23:06 +0100

    Done with static pages

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,6 @@
 TestApp::Application.routes.draw do
   get "pages/home"
-
   get "pages/contact"
-  
   get "pages/about"
 
   resources :microposts
--- spec/controllers/pages_controller_spec.rb
+++ spec/controllers/pages_controller_spec.rb
@@ -3,6 +3,7 @@ require 'spec_helper'
 describe PagesController do
 
   describe "GET 'home'" do
+    render_views
     it "should be successful" do
       get 'home'
       response.should be_success
@@ -10,12 +11,12 @@ describe PagesController do
     
     it "should have the right title" do
       get 'home'
-      response.should have_selector("title",
-                        :content => "Ruby on Rails Tutorial Sample App | Home")
+      response.should have_selector("title", :content => "Ruby on Rails Tutorial Sample App | Home")
     end
   end
 
   describe "GET 'contact'" do
+  	render_views
     it "should be successful" do
       get 'contact'
       response.should be_success
@@ -23,12 +24,12 @@ describe PagesController do
     
     it "should have the right title" do
       get 'contact'
-      response.should have_selector("title",
-                        :content => "Ruby on Rails Tutorial Sample App | Contact")
+      response.should have_selector("title", :content => "Ruby on Rails Tutorial Sample App | Contact")
     end
   end
 
 	describe "GET 'about'" do
+		render_views
     it "should be successful" do
       get 'about'
       response.should be_success
@@ -36,8 +37,7 @@ describe PagesController do
     
     it "should have the right title" do
       get 'about'
-      response.should have_selector("title",
-                        :content => "Ruby on Rails Tutorial Sample App | About")
+      response.should have_selector("title", :content => "Ruby on Rails Tutorial Sample App | About")
     end
   end
 
