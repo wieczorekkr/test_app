a6c3bfb13b780da13190d376fd08ddc1b8bc3a1c
mav <wieczorekkr@gmail.com>
2012-01-08 16:51:10 +0100

    Done with sign in

--- config/routes.rb
+++ config/routes.rb
@@ -1,19 +1,23 @@
 TestApp::Application.routes.draw do
 
+  get "sessions/new"
+
   get "pages/home"
   get "pages/contact"
   get "pages/about"
   get "pages/help"
 
   resources :microposts
-
   resources :users
+  resources :sessions, :only => [:new, :create, :destroy]
   
   match '/contact', :to => 'pages#contact'
   match '/about',   :to => 'pages#about'
   match '/help',    :to => 'pages#help'
   
   match '/signup',  :to => 'users#new'
+  match '/signin',  :to => 'sessions#new'
+  match '/signout', :to => 'sessions#destroy'
 
   # The priority is based upon order of creation:
   # first created -> highest priority.
