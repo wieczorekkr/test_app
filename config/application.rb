eb9c308f745ff48551e382795d7558abc5654048
mav <wieczorekkr@gmail.com>
2012-01-08 15:09:26 +0100

    User signup complete

--- config/application.rb
+++ config/application.rb
@@ -38,5 +38,6 @@ module TestApp
 
     # Configure sensitive parameters which will be filtered from the log file.
     config.filter_parameters += [:password]
+    #config.filter_parameters += [:password, :secret_code]
   end
 end
